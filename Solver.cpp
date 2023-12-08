5e1882b7ca79513d595f27d680432ccdafea449a
HatWithPins <daniel.carril4@gmail.com>
2023-12-08 13:55:22 +0100

    Added SOR algorithm

--- Solver.cpp
+++ Solver.cpp
@@ -1,6 +1,7 @@
 #include "Solver.h"
+#include <cmath>
 
-double pi = 3.14159265359;
+#define PI  3.14159265359
 //Definición de b
 //Define b
 double* defineB(bool isPolar, double boundaryBottom, double boundaryArc, int N, int M)
@@ -24,7 +25,7 @@ double* defineB(bool isPolar, double boundaryBottom, double boundaryArc, int N, 
 			row = i / (M-1);
 			col = i % (M-1);
 			r_i = (N - 1.0 - row)/N;
-			k = r_i * pi / M;
+			k = r_i * PI / M;
 			b[i] = (col == 0)*(1 / (h * h) - 1 / (2 * h * r_i)) * boundaryBottom + (col == M - 2)* (1 / (h * h) + 1 / (2 * h * r_i)) * boundaryBottom + 1 / (r_i * r_i * k * k) * boundaryArc * (row == 0) + (row == N - 2) * (1 / (h * h) - 1 / (2 * h * r_i)) * boundaryBottom;
 		}
 	}
@@ -68,7 +69,7 @@ double* defineA(bool isPolar, int N, int M)
 			row = i / cols;
 			col = i % cols;
 			r_i = (N - 1.0 - row) / N;
-			k = r_i * pi / M;
+			k = r_i * PI / M;
 			A[i] = (col == row) * (2 * (1 / (h * h) + 1 / (r_i * r_i * k * k))) //Diagonal element.
 				+ (row - col == 1 && row % (M - 1) != 0) * (1 / (2 * h * r_i) - 1 / (h * h)) //Left point.
 				- (col - row == 1 && row % (M - 2) != 0) * (1 / (2 * h * r_i) + 1 / (h * h)) //Right point.
@@ -98,12 +99,41 @@ double* defineA(bool isPolar, int N, int M)
 }
 //Algoritmo SOR
 //SOR algotithm
-double* SOR(double* A, double* b, double* x, int N, int M)
+double* SOR(double* A, double* b, double* x0, int N, int M)
 {
 	int sizeX = (N - 1) * (M - 1);
-	double* x_i;
+	double* x1 = new double[sizeX];
 
-	return x_i;
+	double omega = 4 / (2 + sqrt(4 - (cos(PI / M) + cos(PI / N) * (cos(PI / M) + cos(PI / N)))));
+	double sigma;
+	double difference;
+	double TOL = 0.000001;
+	int maxIterations = 1000;
+
+	for (int k = 0; k < maxIterations; k++)
+	{
+		for (int i = 0; i < sizeX; i++)
+		{
+			sigma = 0.0;
+			for (int j = 0; j < sizeX; j++)
+			{
+				if (j != i)
+				{
+					sigma += A[j+i*sizeX] * x0[j];
+				}
+			}
+			x1[i] = (1 - omega) * x0[i] + omega * (b[i] - sigma) / A[i + i * sizeX];
+		}
+		difference = 0.0;
+		for (int i = 0; i < sizeX; i++)
+		{
+			difference += (x1[i] - x0[i]) * (x1[i] - x0[i]);
+		}
+		difference = sqrt(difference);
+		if (difference < TOL) break;
+	}
+
+	return x1;
 }
 //Función para escribir los resultados.
 //Funtion to write results.
